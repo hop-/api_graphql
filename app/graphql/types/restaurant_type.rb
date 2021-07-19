f1b2599f294db6c7a0e10c66f396d7e3354ccaf1
hop- <hovhannespalyan@gmail.com>
2021-07-19 19:23:39 +0400

    Add restaurant type

---
+++ app/graphql/types/restaurant_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class RestaurantType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :location, String, null: true
+    field :phone, String, null: true
+  end
+end
